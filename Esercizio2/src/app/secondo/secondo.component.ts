38f970f1016af30e7fc1e528d3dde3668aaa2fed
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-01-11 13:59:12 +0000

    commit 2

--- Esercizio2/src/app/secondo/secondo.component.ts
+++ Esercizio2/src/app/secondo/secondo.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
-
+import { QuartoComponent } from '../quarto/quarto.component';
+import { TerzoComponent } from '../terzo/terzo.component';
 @Component({
   selector: 'app-secondo',
   standalone: true,
-  imports: [],
+  imports: [QuartoComponent, TerzoComponent],
   templateUrl: './secondo.component.html',
   styleUrl: './secondo.component.css'
 })
